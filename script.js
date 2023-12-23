919f08df4dd18323293869332d5f979b910ada9e
hriidzhridoy <amamunhridoy@gmail.com>
2023-12-23 12:08:29 +0600

    last changes

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-// gsap.registerPlugin(ScrollTrigger)
-
-
-// // gsap.from("#nav img, #nav h3 , #nav button",{
-// //     duration: 0.2,
-// //     delay : 0.1,
-// //     y : -200,
-// //     stagger : 0.5,
-// //     opacity : 0,
-// // })
-
 let tl = gsap.timeline();
 
 tl.from("#nav img, #nav h3 , #nav button",{
         duration: 0.2,
         delay : 0.1,
-        y : -200,
+        y : -100,
         stagger : 0.5,
         opacity : 0,
     })
 
 tl.from(".main h1",{
-    y: -15,
-    opacity: 0
+    y: 150,
+    opacity: 0,
+    stagger: 0.3
 })    
 
-tl.from("#left-pic",{
+tl.from(".main>img",{
+    scale : 0,
+    opacity : 0
+})
+
+tl.from("h5",{
     opacity: 0,
-    yoyo:true,
-    y : 100,
+    scale : 0,
+})
+
+tl.to("h5",{
+    y : 20,
+    repeat : -1,
+    duration: 0.7,
+    yoyo : true,   
 })
