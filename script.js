9d8c8a35f0701e521a162a8780397fd089505584
hriidzhridoy <amamunhridoy@gmail.com>
2023-12-23 11:41:21 +0600

    few changes

--- script.js
+++ script.js
@@ -9,23 +9,23 @@
 // //     opacity : 0,
 // // })
 
-// let tl = gsap.timeline();
+let tl = gsap.timeline();
 
-// tl.from("#nav img, #nav h3 , #nav button",{
-//         duration: 0.2,
-//         delay : 0.1,
-//         y : -200,
-//         stagger : 0.5,
-//         opacity : 0,
-//     })
+tl.from("#nav img, #nav h3 , #nav button",{
+        duration: 0.2,
+        delay : 0.1,
+        y : -200,
+        stagger : 0.5,
+        opacity : 0,
+    })
 
-// tl.from(".main h1",{
-//     y: -15,
-//     opacity: 0
-// })    
+tl.from(".main h1",{
+    y: -15,
+    opacity: 0
+})    
 
-// tl.from("#left-pic",{
-//     opacity: 0,
-//     yoyo:true,
-//     y : 100,
-// })
+tl.from("#left-pic",{
+    opacity: 0,
+    yoyo:true,
+    y : 100,
+})
